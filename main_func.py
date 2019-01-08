1d4b7124aa136345c9f7b90c3ab84fd0655bb69a
jm00445 <jessicamacleod@kubrickgroup.com>
2019-01-08 09:33:41 +0000

    main_function upload

---
+++ main_func.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+'''My Module'''
+
+
+import numpy as np
+import pandas as pd
+import json
+
+
+def main():
+	'''
+	Author: Jessica Macleod
+	Date: 2019-01-08
+	Purpose: I am learning variables this is my main function
+	---
+	Ammednded On: 2019-04-01
+	Ammednded By: Dave
+	Issue Ticket No.: 78512
+	'''
+	myname = 'Lawrence'
+	print(myname)
+
+#execute my main function 
+main()
+	
